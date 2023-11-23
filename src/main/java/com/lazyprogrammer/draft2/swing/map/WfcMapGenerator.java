0ebf36ea3e133d8a86313ef340fcadd3c118fdf0
Gabor <bene.gabor.web@gmail.com>
2023-11-23 21:34:01 +0100

    add random coord picking to WfcMapGenerator

--- src/main/java/com/lazyprogrammer/draft2/swing/map/WfcMapGenerator.java
+++ src/main/java/com/lazyprogrammer/draft2/swing/map/WfcMapGenerator.java
@@ -3,9 +3,14 @@ package com.lazyprogrammer.draft2.swing.map;
 import com.lazyprogrammer.draft2.swing.data.GameMap;
 import com.lazyprogrammer.draft2.swing.data.Terrain;
 import com.lazyprogrammer.draft2.swing.data.TileAttribute;
+import lombok.extern.slf4j.Slf4j;
 
 import java.awt.Point;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
+@Slf4j
 public class WfcMapGenerator implements MapGenerator {
 
   @Override
@@ -13,14 +18,57 @@ public class WfcMapGenerator implements MapGenerator {
     var gameMap = new GameMap(mapConfig);
     final var columnNo = mapConfig.columnNo();
     final var rowNo = mapConfig.rowNo();
+    var usedCoordinates = initOceansAtTheEdge(columnNo, gameMap, rowNo);
+    var availableCoordinates = new HashSet<>(gameMap.getCoordinates());
+    availableCoordinates.removeAll(usedCoordinates);
+    var coord = pickRandomCoord(availableCoordinates);
+    log.info("{}", gameMap.read(coord, TileAttribute.TERRAIN));
+    return gameMap;
+  }
+
+  private static Point pickRandomCoord(HashSet<Point> availableCoordinates) {
+    int randomIndex = ThreadLocalRandom.current()
+                                       .nextInt(availableCoordinates.size());
+    int i = 0;
+    for (Point point : availableCoordinates) {
+      if (i == randomIndex) {
+        return point;
+      }
+      i++;
+    }
+    throw new AssertionError("We shouldn't be here!");
+  }
+
+  private static Set<Point> initOceansAtTheEdge(int columnNo, GameMap gameMap, int rowNo) {
+    Set<Point> usedCoordinates = new HashSet<>();
     for (int i = 0; i < columnNo; i++) {
-      gameMap.write(new Point(i, 0), TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
-      gameMap.write(new Point(i, 1), TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
+      var p1 = new Point(i, 0);
+      gameMap.write(p1, TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
+      var p2 = new Point(i, 1);
+      gameMap.write(p2, TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
+      var p3 = new Point(i, rowNo - 2);
+      gameMap.write(p3, TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
+      var p4 = new Point(i, rowNo - 1);
+      gameMap.write(p4, TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
+      usedCoordinates.add(p1);
+      usedCoordinates.add(p2);
+      usedCoordinates.add(p3);
+      usedCoordinates.add(p4);
     }
     for (int j = 0; j < rowNo; j++) {
-      gameMap.write(new Point(0, j), TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
-      gameMap.write(new Point(1, j), TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
+      var p1 = new Point(0, j);
+      gameMap.write(p1, TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
+      var p2 = new Point(1, j);
+      gameMap.write(p2, TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
+      var p3 = new Point(columnNo - 2, j);
+      gameMap.write(p3, TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
+      var p4 = new Point(columnNo - 1, j);
+      gameMap.write(p4, TileAttribute.TERRAIN, Terrain.OCEAN.ordinal());
+      usedCoordinates.add(p1);
+      usedCoordinates.add(p2);
+      usedCoordinates.add(p3);
+      usedCoordinates.add(p4);
     }
-    return gameMap;
+    return usedCoordinates;
   }
 }
